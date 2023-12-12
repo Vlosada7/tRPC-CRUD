ffad203309044e80f01b6574ee5a705d2f2f6b14
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-12-12 14:21:47 +0100

    trpc config and first route done

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
 import express from "express";
+import * as trpcExpress from "@trpc/server/adapters/express";
 import morgan from "morgan";
+import { router, createContext } from "./trpc";
+import { notesRouter } from "./routes/notes";
 
 const app = express();
+
+const appRouter = router({
+	note: notesRouter,
+});
 app.use(morgan("dev"));
+
+app.use(
+	"/trpc",
+	trpcExpress.createExpressMiddleware({
+		router: appRouter,
+		createContext,
+	})
+);
 
 export default app;
