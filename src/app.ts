cf605bb5a37a6fe0a1da781598b542f8079c63b1
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-12-12 15:25:55 +0100

    Client working and conected with Backend

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import * as trpcExpress from "@trpc/server/adapters/express";
 import morgan from "morgan";
 import { router, createContext } from "./trpc";
 import { notesRouter } from "./routes/notes";
+import cors from "cors";
 
 const app = express();
 
 const appRouter = router({
 	note: notesRouter,
 });
+app.use(cors());
 app.use(morgan("dev"));
 
 app.use(
@@ -18,5 +20,7 @@ app.use(
 		createContext,
 	})
 );
+
+export type AppRouter = typeof appRouter;
 
 export default app;
--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -1,7 +1,13 @@
 import { publicProcedure, router } from "../trpc";
 
 export const getNotes = publicProcedure.query(() => {
-	return [];
+	return [
+		{
+			id: 1,
+			title: "Note 1",
+			content: "Content 1",
+		},
+	];
 });
 
 export const notesRouter = router({
